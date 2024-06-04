4063e22e7454d9b631a6ca2b86faaae760fc331b
Ekaterina Bondareva <1978231@bk.ru>
2024-06-04 13:57:30 +0500

    created shortAnswer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { questionData } from "./QuestionData";
 import { Multichoice } from "./components/Multichoice";
 import { Onechoice } from "./components/Onechoice";
+import { ShortAnswer } from "./components/ShortAnswer";
+
 export function App() {
   const [currentQuestionId, setCurrentQuestionId] = useState<number>(1);
   function currentQuestionInformation() {
@@ -11,7 +13,9 @@ export function App() {
 
   return currentQuestionInformation()?.type==="multichoice" ? (<Multichoice/>):(
     currentQuestionInformation()?.type === 'onechoice' ?(<Onechoice/>):(
-      
+currentQuestionInformation()?.type === 'shortAnswer' ?(<ShortAnswer/>):(
+  
+)
     )
   )
 }
