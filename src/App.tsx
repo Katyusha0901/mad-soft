64619706f1e943d9925e3c07116c4b9332849942
Ekaterina Bondareva <1978231@bk.ru>
2024-06-04 13:52:00 +0500

    created component for multiple choice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useState } from "react";
+import { questionData } from "./QuestionData";
+import { Multichoice } from "./components/Multichoice";
 
 export function App() {
-  return <div className="App"></div>;
+  const [currentQuestionId, setCurrentQuestionId] = useState<number>(1);
+  function currentQuestionInformation() {
+    return questionData.find((object) => object.id === currentQuestionId);
+  }
+
+  return currentQuestionInformation()?.type==="multichoice" ? (<Multichoice/>):()
 }
---
+++ src/components/Multichoice.tsx
@@ -0,0 +1,3 @@
+export function Multichoice() {
+  return <div></div>;
+}
