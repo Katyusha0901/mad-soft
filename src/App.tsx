abee65de31bcd68df1d89031d394ab8c5c3f433b
Ekaterina Bondareva <1978231@bk.ru>
2024-06-04 13:55:04 +0500

    created one choice component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useState } from "react";
 import { questionData } from "./QuestionData";
 import { Multichoice } from "./components/Multichoice";
-
+import { Onechoice } from "./components/Onechoice";
 export function App() {
   const [currentQuestionId, setCurrentQuestionId] = useState<number>(1);
   function currentQuestionInformation() {
     return questionData.find((object) => object.id === currentQuestionId);
   }
 
-  return currentQuestionInformation()?.type==="multichoice" ? (<Multichoice/>):()
+  return currentQuestionInformation()?.type==="multichoice" ? (<Multichoice/>):(
+    currentQuestionInformation()?.type === 'onechoice' ?(<Onechoice/>):(
+      
+    )
+  )
 }
